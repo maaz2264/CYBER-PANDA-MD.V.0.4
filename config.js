4a17975c2613693e3cfd621ff9581cc5bd0c9a08
maaz2264 <143191256+maaz2264@users.noreply.github.com>
2024-04-21 13:03:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
+global.Owner = ["+923192106398", ""];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923192106398", ""];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923192106398"];
+global.OwnerName = "Maazxox";
 global.BotName = "SACHITH";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
